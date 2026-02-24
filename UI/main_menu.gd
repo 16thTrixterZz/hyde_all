0989715f791eb99ba57a29856f25218d681927b5
16thTrixterZz <ricoalf19@gmail.com>
2026-02-24 21:37:38 +0800

    Add files via upload

---
+++ UI/main_menu.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var UI: int = 1
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_training_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_vs_mode_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_arcade_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	pass # Replace with function body.
